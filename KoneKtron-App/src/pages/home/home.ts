3a0ee7beb22d2d5657fa8bd53cb6cdc020bce8ed
Ivam Galvão Filho <ivam.developer@gmail.com>
2018-10-03 19:11:40 -0300

    Second_commit

--- KoneKtron-App/src/pages/home/home.ts
+++ KoneKtron-App/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProfilePage } from '../profile/profile';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,10 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  profile() {
+    this.navCtrl.push(ProfilePage);
+  }
+  logout() {
+    this.navCtrl.setRoot(LoginPage);
+  }
 }
